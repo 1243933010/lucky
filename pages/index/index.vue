56c3f7321ff2dad961435bcf0a92b52333a58e93
ysys <1243933010@qq.com>
2024-08-22 17:38:14 +0800

    feat()；完成首页切图

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -121,10 +121,12 @@
 		</view>
 		<view class="uni-margin-wrap" style="margin-bottom: 173rpx;">
 			<!-- 轮播图 -->
-			<swiper class="banner-container" :indicator-dots="false" :autoplay="false" :circular="false" previous-margin="120rpx" next-margin="120rpx" @change="swipterChange" :current="swipterActive">
+			<swiper class="banner-container" :indicator-dots="false" :autoplay="false" :circular="false"
+				previous-margin="120rpx" next-margin="120rpx" @change="swipterChange" :current="swipterActive">
 				<block v-for="(item,index) in swiperData" :key="index">
-					<swiper-item class="banner-item" >
-						<view class="banner-box" :class="swipterActive==index-1?'previous':swipterActive==index+1?'next':''">
+					<swiper-item class="banner-item">
+						<view class="banner-box"
+							:class="swipterActive==index-1?'previous':swipterActive==index+1?'next':''">
 							<image mode="widthFix" class="banner-img" :src="item.imgUrl"></image>
 						</view>
 					</swiper-item>
@@ -152,6 +154,64 @@
 				</view>
 			</view>
 		</view>
+		<view class="hr3">
+			<image src="../../static/u4.png" mode="widthFix"></image>
+		</view>
+		<view class="Primary">
+			<view class="title">
+				<text>friend'S</text>
+			</view>
+			<view class="label">
+				<text>Create a friend room, invite your friends to play together or join your friend room Create a
+					friend room, invite your friends to play together or join your friend room Create a friend room,
+					invite your friends to play together or join your friend room</text>
+			</view>
+		</view>
+		<view class="create">
+			<view class="left">
+				<image src="../../static/u5.png" mode="widthFix"></image>
+			</view>
+			<view class="right">
+				<view class="join">
+					<image src="../../static/u6.png" mode="widthFix"></image>
+				</view>
+				<view class="btn">
+					<view class="">
+						<text>+</text>
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="h4"></view>
+		<view class="faq">
+			<view class="title">
+				<text>FAQS</text>
+				<image src="../../static/right_arrow.png" mode="widthFix"></image>
+			</view>
+			<view class="faq-box">
+				<view class="list">
+					<view class="item" :class="item.show?'show':'hide'" v-for="(item,index) in faqList" :key="index">
+						<text style="color: white;">{{item.text}}</text>
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="foot">
+			<view class="logo">
+				<text>LOGO</text>
+			</view>
+			<view class="bottom">
+				<view class="link">
+					<text>Twitter</text><text>/</text>
+					<text>Tele.</text><text>/</text>
+					<text>Disco.</text><text>/</text>
+					<text>Git.</text>
+				</view>
+				<view class="img">
+					<image src="../../static/u7.png" mode="widthFix"></image>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -170,16 +230,81 @@
 						imgUrl: '../../static/index_bk.png'
 					},
 				],
-				swipterActive:0
+				swipterActive: 0,
+				faqList: [{
+						text: '11111',
+						id: 1,
+						show: true
+					},
+					{
+						text: '2222',
+						id: 2,
+						show: true
+					},
+					{
+						text: '3333',
+						id: 3,
+						show: true
+					},
+					{
+						text: '4444',
+						id: 4,
+						show: true
+					},
+				],
+				colorList: ["#3e92cc", "#f2a91f", "#a612ee", "#07c34d", "#56d894", "#ffa4bb", "#e8cd22", "#4f32cc",
+					"#ffcc11", "#0e9c5a"
+				]
+
 			}
 		},
 		onLoad() {
-
+			this.faqList.forEach((val, ind) => {
+				val.color = this.colorList[Math.min(10, this.getRandomInt(1, 10))]
+			})
+			// console.log(Math.min(10, this.getRandomInt(1, 10)))
+			this.startToggle();
 		},
 		methods: {
-			swipterChange(e){
+			swipterChange(e) {
 				this.swipterActive = e.detail.current;
-			}
+			},
+			// 开始每2秒执行一次切换
+			startToggle() {
+				setInterval(() => {
+					// 计算要切换状态的元素个数（1-3个）
+					const count = this.getRandomInt(1, 3);
+
+					// 随机获取要切换的元素
+					const itemsToToggle = this.getRandomElements(this.faqList, count);
+
+					// 切换状态
+					itemsToToggle.forEach(item => {
+						item.show = !item.show; // 反转当前的show值
+					});
+				}, 2000);
+			},
+
+			// 获取min-max之间的随机整数
+			getRandomInt(min, max) {
+				return Math.floor(Math.random() * (max - min + 1)) + min;
+			},
+
+			// 随机从数组中选择count个不同的元素
+			getRandomElements(arr, count) {
+				const result = [];
+				const usedIndices = new Set();
+
+				while (result.length < count) {
+					const index = Math.floor(Math.random() * arr.length);
+					if (!usedIndices.has(index)) {
+						result.push(arr[index]);
+						usedIndices.add(index);
+					}
+				}
+
+				return result;
+			},
 		}
 	}
 </script>
@@ -189,16 +314,23 @@
 
 	page {
 		height: 100%;
-		background-color: #23222d;
-		// background: linear-gradient( 0deg, #040405 0%, #23212c 100%);
+		// background-color: #23222d;
+		// background: linear-gradient(to bottom, #23212c,#040405);
+		// background: linear-gradient( 141deg, #292734 0%, #000000 100%);
+		// background: linear-gradient(to bottom, #ff7e5f, #feb47b, #86a8e7);
+
 		// background: #201f29 url('../../static/login_bk.png') no-repeat 100% 100%;
+	}
+
+	.page-container {
+		background: linear-gradient(141deg, #292734 0%, #000000 100%);
 	}
 
 	/* 轮播图 */
 	.banner-container {
 		width: 100%;
 		height: 555rpx;
-		
+
 	}
 
 	.banner-item {
@@ -213,19 +345,21 @@
 		height: 100%;
 		padding-left: 10%;
 	}
-	.previous{
+
+	.previous {
 		transform: rotate(10deg) !important;
-		 transition: transform 0.7s ease;
-		 position: absolute;
-		 right: -72rpx;
+		transition: transform 0.7s ease;
+		position: absolute;
+		right: -72rpx;
 		// padding-top: 20rpx;
 		// padding-right: 120rpx;
 	}
-	.next{
+
+	.next {
 		transform: rotate(-10deg) !important;
-		 transition: transform 0.7s ease;
-		 position: absolute;
-		 left: -50rpx;
+		transition: transform 0.7s ease;
+		position: absolute;
+		left: -50rpx;
 	}
 
 	.banner-img {
@@ -528,6 +662,7 @@
 		border-radius: 25rpx;
 		padding-bottom: 27rpx;
 		margin-bottom: 52rpx;
+
 		// border: 1px solid #C2C2C2;
 		// border-image: linear-gradient(180deg, rgba(68.00000354647636, 68.00000354647636, 68.00000354647636, 1), rgba(0, 0, 0, 0)) 2 2;
 		.container {
@@ -600,40 +735,50 @@
 			}
 		}
 	}
-    .Falling{
-		.container{
+
+	.Falling {
+		margin-bottom: 69rpx;
+
+		.container {
 			width: 680rpx;
 			// height: 208rpx;
 			margin: 0 auto;
-			background: linear-gradient( 146deg, #555555 0%, #000000 100%);
-			box-shadow: inset 7rpx 7rpx 28rpx 0rpx rgba(84,84,84,0.2118);
+			background: linear-gradient(146deg, #555555 0%, #000000 100%);
+			box-shadow: inset 7rpx 7rpx 28rpx 0rpx rgba(84, 84, 84, 0.2118);
 			border-radius: 28rpx;
 			border: 2rpx solid #666666;
 			box-sizing: border-box;
 			padding: 22rpx 26rpx;
 			.flex-space-between;
-			.logo{
+
+			.logo {
 				width: 142rpx;
 				margin-right: 20rpx;
-				image{
+
+				image {
 					width: 100%;
 				}
 			}
-			.right{
+
+			.right {
 				width: calc(100% - 71rpx);
 				display: flex;
 				flex-direction: column;
 				justify-content: space-between;
 				flex-grow: 1;
-				.text{
+
+				.text {
 					display: flex;
 					flex-direction: column;
 					margin-bottom: 20rpx;
-					.title{
+
+					.title {
 						color: #FFFFFF;
 						font-size: 35rpx;
+						margin-bottom: 10rpx;
 					}
-					.label{
+
+					.label {
 						background-image: linear-gradient(55.53466052546843deg, #9DFE00 0%, #14D9E5 100%);
 						-webkit-background-clip: text;
 						background-clip: text;
@@ -641,20 +786,197 @@
 						font-size: 35rpx;
 					}
 				}
-				.bottom{
+
+				.bottom {
 					width: 100%;
 					display: flex;
 					justify-content: space-between;
 					align-items: center;
-					text{
+
+					text {
 						color: #AAAAAA;
 						font-size: 27rpx;
 						.flex-center;
 						line-height: 1;
 					}
-					image{
+
+					image {
 						width: 11rpx;
 					}
+				}
+			}
+		}
+	}
+
+	.hr3 {
+		width: 100%;
+		border-top: 1px dashed #38D1DC;
+		border-bottom: 1px dashed #38D1DC;
+		margin-bottom: 95rpx;
+
+		image {
+			width: 100%;
+		}
+	}
+
+	.Primary {
+		width: 100%;
+		box-sizing: border-box;
+		padding: 0 38rpx;
+		margin-bottom: 100rpx;
+
+		.title {
+			color: white;
+			font-size: 69rpx;
+			letter-spacing: 0rpx;
+			font-weight: 600;
+			margin-bottom: 30rpx;
+			// font-weight: ;
+		}
+
+		.label {
+			color: #C2C2C2;
+			font-size: 24rpx;
+			line-height: 2;
+		}
+	}
+
+	.create {
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		margin-bottom: 53rpx;
+
+		.left {
+			width: 502rpx;
+
+			image {
+				width: 100%;
+			}
+		}
+
+		.right {
+			.flex-column;
+
+			.join {
+				width: 130rpx;
+
+				image {
+					width: 100%;
+				}
+			}
+
+			.btn {
+				width: 140rpx;
+				height: 294rpx;
+				border-radius: 87rpx;
+
+				// border: 9rpx solid ;
+				background: linear-gradient(146deg, #9DFE00 0%, #14D9E5 100%);
+				.flex-center;
+
+				view {
+					width: 131rpx;
+					height: 285rpx;
+					background-color: #030304;
+					border-radius: 87rpx;
+					.flex-center;
+
+					text {
+						font-size: 80rpx;
+						color: white;
+						font-weight: 600;
+					}
+
+					// border-radius: 87rpx;
+				}
+			}
+		}
+	}
+
+	.h4 {
+		width: 100%;
+		height: 1.7px;
+		background-color: #333333;
+		margin-bottom: 65rpx;
+	}
+
+	.faq {
+		box-sizing: border-box;
+		padding: 0 38rpx;
+		margin-bottom: 118rpx;
+
+		.title {
+			font-size: 69rpx;
+			color: white;
+			font-weight: 600;
+			line-height: 1;
+			margin-bottom: 98rpx;
+			.flex-space-between;
+
+			image {
+				width: 12rpx;
+			}
+		}
+
+		.list {
+			display: flex;
+			flex-direction: row;
+			flex-wrap: wrap;
+
+			.item {
+				background: #1D1B26;
+				// background: red;
+				border-radius: 69rpx;
+				margin-bottom: 38rpx;
+				margin-right: 7rpx;
+				.flex-center;
+				height: 71rpx;
+				padding: 0rpx 75rpx;
+				 position: relative;
+				    transition: opacity 0.7s ease, visibility 0.7s ease;
+			}
+
+			.show {
+				opacity: 1;
+				    visibility: visible;
+				    position: relative;
+			}
+
+			.hide {
+				opacity: 0;
+				    visibility: hidden;
+				    position: absolute;
+			}
+		}
+	}
+
+	.foot {
+		box-sizing: border-box;
+		padding: 0 36rpx 60rpx 52rpx;
+
+		.logo {
+			color: white;
+			font-size: 48rpx;
+		}
+
+		.bottom {
+			font-size: 27rpx;
+			color: white;
+			.flex-space-between;
+
+			.link {
+				.flex-direction;
+
+				text {}
+			}
+
+			.img {
+				width: 123rpx;
+
+				image {
+					width: 100%;
 				}
 			}
 		}
---
+++ pages/me/recharge.vue
@@ -0,0 +1,263 @@
+<template>
+	<view class="page-container">
+		<DefaultHeader />
+		<view class="recharge">
+			<view class="click">
+				<view class="recharge-btn">
+					<text>Recharge</text>
+					<image src="../../static/recharge.png" mode="widthFix"></image>
+				</view>
+				<view class="logo">
+					<image src="../../static/logo.png" mode="widthFix"></image>
+				</view>
+				<view class="withdraw">
+
+					<image src="../../static/withdraw.png" mode="widthFix"></image>
+					<text>Withdraw</text>
+				</view>
+			</view>
+			<view class="recharge-container">
+				<view class="header">
+					<view class="title">
+						<text>Recharge</text>
+					</view>
+					<view class="icon">
+						<image src="../../static/order.png" mode="widthFix"></image>
+					</view>
+				</view>
+				<view class="content">
+					<view class="input">
+						<image src="../../static/me_icon.png" mode="widthFix"></image>
+						<view class="input-con">
+							<input type="number" />
+						</view>
+					</view>
+					<view class="radio">
+						<view class="title">
+							<text>Choose Payment Method</text>
+						</view>
+						<view class="radio-con">
+							<view class="radio-item">
+								<view class="label">
+									<text>USDT-TRC20</text>
+								</view>
+								<view class="btn" @click="chooseRadio(0)">
+									<view class="border">
+										<view class="active" v-if="radioIndex==0"></view>
+									</view>
+								</view>
+							</view>
+							<view class="radio-item">
+								<view class="label">
+									<text>USDT-BNB Smart Chain/BEP20</text>
+								</view>
+								<view class="btn" @click="chooseRadio(1)">
+									<view class="border">
+										<view class="active" v-if="radioIndex==1"></view>
+									</view>
+								</view>
+							</view>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import DefaultHeader from '../../components/defaultHeader.vue';
+	import DefaultFooter from '../../components/defaultFooter.vue';
+	export default {
+		components: {
+			DefaultHeader,
+			DefaultFooter
+		},
+		data() {
+			return {
+				radioIndex: 0
+			};
+		},
+		methods:{
+			chooseRadio(index){
+				this.radioIndex = index;
+			}
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	@import url("../../static/default.less");
+
+	page {
+		height: 100%;
+		// background-color: #040405;
+		// background: linear-gradient( 0deg, #040405 0%, #23212c 100%);
+		background: #201f29 url('../../static/login_bk.png') no-repeat 100% 100%;
+	}
+
+	.recharge {
+		width: 100%;
+		padding-top: 87rpx;
+		box-sizing: border-box;
+		padding-left: 52rpx;
+		padding-right: 52rpx;
+
+		.click {
+			width: 100%;
+			margin-bottom: 127rpx;
+			.flex-space-between;
+
+			.recharge-btn,
+			.withdraw {
+				width: 267rpx;
+				height: 91rpx;
+				background: #222222;
+				box-shadow: -2rpx -2rpx 5rpx 0rpx rgba(255, 255, 255, 0.25);
+				border-radius: 18rpx 18rpx 18rpx 18rpx;
+
+				.flex-direction;
+
+				text {
+					color: #DDDDDD;
+					font-size: 21rpx;
+				}
+
+				image {
+					width: 50rpx;
+				}
+			}
+
+			.recharge-btn {
+				padding-left: 21rpx;
+
+				text {
+					margin-right: 14rpx;
+				}
+			}
+
+			.withdraw {
+				padding-right: 21rpx;
+				display: flex;
+				flex-direction: row;
+				justify-content: flex-end;
+
+				text {
+					margin-left: 14rpx;
+				}
+			}
+
+			.logo {
+				width: 105rpx;
+				margin: 0 32rpx;
+
+				image {
+					width: 100%;
+					border-radius: 50%;
+				}
+			}
+		}
+
+		.recharge-container {
+			width: 100%;
+
+			.header {
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				margin-bottom: 54rpx;
+
+				.title {
+					font-size: 42rpx;
+					color: white;
+				}
+
+				.icon {
+					width: 61rpx;
+					height: 61rpx;
+					background: #222222;
+					box-shadow: 2rpx -2rpx 5rpx 0rpx rgba(255, 255, 255, 0.3);
+					border-radius: 50%;
+					.flex-center;
+
+					image {
+						width: 28rpx;
+					}
+				}
+			}
+
+			.content {
+				width: 100%;
+
+				.input {
+					margin-bottom: 29rpx;
+					.flex-direction;
+
+					image {
+						width: 70rpx;
+						margin-right: 7rpx;
+					}
+
+					.input-con {
+						width: 554rpx;
+						height: 70rpx;
+						background: #111111;
+						border-radius: 18rpx 18rpx 18rpx 18rpx;
+						border: 1rpx solid #999999;
+						color: #999999;
+						.flex-direction;
+						padding-left: 25rpx;
+					}
+				}
+
+				.radio {
+					display: flex;
+					flex-direction: column;
+
+					.title {
+						color: #AAAAAA;
+						font-size: 24rpx;
+						line-height: 1.5;
+						margin-bottom: 14rpx;
+					}
+
+					.radio-con {
+						width: 613rpx;
+						// height: 140rpx;
+						background: linear-gradient(146deg, rgba(68, 68, 68, 0.5) 0%, rgba(0, 0, 0, 0.5) 100%);
+						box-shadow: inset 7rpx 7rpx 28rpx 0rpx rgba(84, 84, 84, 0.2118);
+						border-radius: 25rpx 25rpx 25rpx 25rpx;
+						border: 1rpx solid #666666;
+						box-sizing: border-box;
+						padding: 26rpx 26rpx 0 26rpx;
+						.radio-item {
+							.flex-space-between;
+							box-sizing: border-box;
+							margin-bottom: 26rpx;
+							.label {
+								color: #999999;
+								font-size: 28rpx;
+							}
+
+							.btn {
+								width: 28rpx;
+								height: 28rpx;
+								background: #111111;
+								border-radius: 18rpx 18rpx 18rpx 18rpx;
+								border: 1rpx solid #999999;
+								.flex-center;
+							}
+							.active{
+								width: 11rpx;
+								height: 11rpx;
+								background: #FFFFFF;
+								border-radius: 18rpx 18rpx 18rpx 18rpx;
+							}
+						}
+					}
+				}
+			}
+
+		}
+	}
+</style>
